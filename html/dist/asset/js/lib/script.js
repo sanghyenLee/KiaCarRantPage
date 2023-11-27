3fd016d3e5449fa139d654ba9fb6df778021ff6a
sanghyenLee <alikcz83@gmail.com>
2023-11-27 16:51:24 +0900

    탭메뉴 완성

--- html/dist/asset/js/lib/script.js
+++ html/dist/asset/js/lib/script.js
@@ -9,3 +9,16 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-button-prev",
   },
 });
+$(document).ready(function () {
+  $("ul.tabs li").click(function () {
+    var tab_id = $(this).attr("data-tab");
+
+    $("ul.tabs li").removeClass("current");
+    $(".tab-content").removeClass("current");
+
+    $(this).addClass("current");
+    $("#" + tab_id).addClass("current");
+  });
+
+  //   $(".tab-box .top img").click
+});
