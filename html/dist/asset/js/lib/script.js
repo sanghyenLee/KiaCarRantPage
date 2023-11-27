8a2c5051b2862e62a2bd10dc732a468169bd853e
sanghyenLee <alikcz83@gmail.com>
2023-11-27 12:30:03 +0900

    메인 슬라이드, 셀렉트 센터 부분 완료

---
+++ html/dist/asset/js/lib/script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper-container", {
+  //   direction: "horizontal",
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 3000,
+  //   },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
