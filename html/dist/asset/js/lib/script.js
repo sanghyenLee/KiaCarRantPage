2924aff011c5ab5e42f8865cb7a53f48d3e80d11
sanghyenLee <alikcz83@gmail.com>
2023-11-29 13:41:32 +0900

    밑에 슬라이드 완성

--- html/dist/asset/js/lib/script.js
+++ html/dist/asset/js/lib/script.js
@@ -5,8 +5,22 @@ const swiper = new Swiper(" .swiper-container", {
   //     delay: 3000,
   //   },
   navigation: {
+    nextEl: ".button-container .swiper-button-next",
+    prevEl: ".button-container .swiper-button-prev",
+  },
+});
+const mySwiper = new Swiper(" .mySwiper", {
+  direction: "horizontal",
+  allowTouchMove: false,
+  slidesPerView: "auto",
+
+  navigation: {
     nextEl: " .swiper-button-next",
     prevEl: " .swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
 $(document).ready(function () {
